da0450850b08a5a548927c52ded8e5774e2e79c1
David Dollar <ddollar@gmail.com>
2010-11-10 00:34:57 -0500

    dont need deploy.sh any more

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rsync -lrvuz . piraterad.io:/app/pirateradio --exclude '.git'
-ssh piraterad.io "sudo chown -R root:pirateradio /app/pirateradio"
-ssh piraterad.io "sudo chmod -R g+w /app/pirateradio"
-ssh piraterad.io "sudo restart pirateradio.node"
